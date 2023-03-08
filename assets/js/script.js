8d5a272c237bd8bb43540bc206b16f86f40bbe2f
Weston Baker <westonbaker0@gmail.com>
2023-03-08 13:50:41 -0500

    Semi Functional Search Form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
 var homeLink = document.querySelector(".home-header");
 var charactersLink = document.querySelector(".characters-header");
 var compareLink = document.querySelector(".compare-header");
-var characterInput = document.querySelector(".form");
+var searchForm = document.querySelector(".container-header-search");
+var searchInput = document.querySelector(".container-header-search input");
+var searchList = document.getElementById("search-list");
 
 // Gets the home text element
 var homeText = document.querySelector(".home-text");
 
-// Hides certian elements by default
+// Hides certain elements by default
 homeLink.style.display = "none";
-characterInput.style.display = "none";
+searchForm.style.display = "none";
 
 // Adds event listener to the characters link
 charactersLink.addEventListener("click", () => {
     homeLink.style.display = "inline";
     charactersLink.style.display = "none";
-    characterInput.style.display = "block";
+    searchForm.style.display = "block";
 });
 
 // Adds event listener to the compare link
@@ -26,6 +28,51 @@ compareLink.addEventListener("click", () => {
 homeLink.addEventListener("click", () => {
     location.replace(location.href);
     homeLink.style.display = "none";
-    characterInput.style.display = "none";
-    charactersLink.style.display = "inline"
+    charactersLink.style.display = "inline";
 });
+
+function searchCharacter(searchText) {
+    const url = `https://www.superheroapi.com/api.php/2333600500149305/search/${searchText}`
+    fetch(url)
+      .then(response => response.json())
+      .then((jsonData) => {
+        const results = jsonData.results.map(element => {
+          return {
+            name: element.name,
+            image: element.image.url
+          }
+        });
+        renderResults(results);
+        console.log(results);
+      });
+  }
+  
+  function renderResults(results) {
+    searchList.innerHTML = ""; // clear previous results
+    results.forEach(result => {
+      const searchElement = document.createElement("li");
+      searchElement.innerText = result.name;
+      
+      const imageElement = document.createElement("img");
+      imageElement.src = result.image;
+      imageElement.style.width = "50px"; // set image width
+      
+      const listItemElement = document.createElement("div");
+      listItemElement.appendChild(imageElement);
+      listItemElement.appendChild(searchElement);
+      
+      listItemElement.addEventListener("click", () => {
+          // do something when list item is clicked
+          console.log(`You clicked on ${result.name}`);
+      });
+      
+      searchList.appendChild(listItemElement);
+    });
+  }
+searchInput.addEventListener("keyup", () => {
+    searchCharacter(searchInput.value);
+});
+
+window.onload = () => {
+    renderResults([]);
+};
