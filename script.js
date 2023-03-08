32212f821dfa14c51bc96ff16ba29c929b9c654c
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-03-08 15:03:52 -0500

    foundation working on api

---
+++ script.js
@@ -0,0 +1,33 @@
+var homeLink = document.querySelector(".home-header");
+var charactersLink = document.querySelector(".characters-header");
+var compareLink = document.querySelector(".compare-header");
+var characterInput = document.querySelector(".form");
+
+// Gets the home text element
+var homeText = document.querySelector(".home-text");
+
+// Hides certian elements by default
+homeLink.style.display = "none";
+characterInput.style.display = "none";
+
+// Adds event listener to the characters link
+charactersLink.addEventListener("click", () => {
+    homeLink.style.display = "inline";
+    charactersLink.style.display = "none";
+    characterInput.style.display = "block";
+});
+
+// Adds event listener to the compare link
+compareLink.addEventListener("click", () => {
+    alert("Sorry, comparison feature is not available yet. Stay tuned!");
+});
+
+// Adds event listener to the home link
+homeLink.addEventListener("click", () => {
+    location.replace(location.href);
+    homeLink.style.display = "none";
+    characterInput.style.display = "none";
+    charactersLink.style.display = "inline"
+});
+
+//API KEY 10223771296377092
