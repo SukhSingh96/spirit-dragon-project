f83ff7a9ba634f824fdb892ef3e77e29e03ac635
CGoddard1 <goddard.cole@gmail.com>
2023-03-12 15:16:36 -0400

    mapbox added

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,3 +86,34 @@ searchInput.addEventListener("keyup", () => {
 window.onload = () => {
   renderResults([]);
 }; 
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ29kZGFyZGNvbGUiLCJhIjoiY2xmNXB3MWg4MDVlZjNwcW1ncXF5b3dncCJ9.0Ft4_LAHrAwshgLHvm4r0Q';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(position) {
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        })
+
+        const nav = new mapboxgl.NavigationControl()
+        map.addControl(nav)
+
+        var directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+          });
+          
+          map.addControl(directions, 'top-left');
+    }
