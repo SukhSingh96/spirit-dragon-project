deeec0883e63957f7ea5bf00764302f064c88c4b
Sukhdip Singh <121911679+SukhSingh96@users.noreply.github.com>
2023-03-14 17:14:27 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,217 @@
-var homeLink = document.querySelector(".home-header");
-var charactersLink = document.querySelector(".characters-header");
-var compareLink = document.querySelector(".compare-header");
-var searchForm = document.querySelector(".container-header-search");
-var searchInput = document.querySelector(".container-header-search input");
-var searchList = document.getElementById("search-list");
+const allTabsBody = document.querySelectorAll('.tab-body-single');
+const allTabsHead = document.querySelectorAll('.tab-head-single');
+const searchForm = document.querySelector('.app-header-search');
+let searchList = document.getElementById('search-list');
 
-// Gets the home text element
-var homeText = document.querySelector(".home-text");
+let activeTab = 1, allData;
 
-// Hides certain elements by default
-homeLink.style.display = "none";
-searchForm.style.display = "none";
+const init = () => {
+    showActiveTabBody();
+    showActiveTabHead();
+}
 
-// Adds event listener to the characters link
-charactersLink.addEventListener("click", () => {
-  homeLink.style.display = "inline";
-  charactersLink.style.display = "none";
-  searchForm.style.display = "block";
+const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
+
+const showActiveTabBody = () => {
+    hideAllTabBody();
+    allTabsBody[activeTab - 1].classList.add('show-tab');
+}
+
+const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
+const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
+
+// even listeners
+window.addEventListener('DOMContentLoaded', () => init());
+// button event listeners
+allTabsHead.forEach(singleTabHead => {
+    singleTabHead.addEventListener('click', () => {
+        hideAllTabHead();
+        activeTab = singleTabHead.dataset.id;
+        showActiveTabHead();
+        showActiveTabBody();
+    });
 });
 
-// Adds event listener to the compare link
-compareLink.addEventListener("click", () => {
-  alert("Sorry, comparison feature is not available yet. Stay tuned!");
-});
+const getInputValue = (event) => {
+    event.preventDefault();
+    let searchText = searchForm.search.value;
+    fetchAllSuperHero(searchText);
+}
 
-// Adds event listener to the home link
-homeLink.addEventListener("click", () => {
-  location.replace(location.href);
-  homeLink.style.display = "none";
-  charactersLink.style.display = "inline";
-});
+// search form submission
+searchForm.addEventListener('submit', getInputValue);
 
-function searchCharacter(searchText) {
-  if (!searchText.trim()) { // check if searchText is empty or only whitespace
-    searchList.innerHTML = "";
-    homeText.style.display = "block"; // show the home text element
-    return;
-  }
-  const url = `https://www.superheroapi.com/api.php/2333600500149305/search/${searchText}`
-  fetch(url)
-    .then(response => response.json())
-    .then((jsonData) => {
-      const results = jsonData.results.map(element => {
-        return {
-          name: element.name,
-          image: element.image.url
+// api key => 727054372039115
+const fetchAllSuperHero = async(searchText) => {
+    let url = `https://www.superheroapi.com/api.php/1216454192525968/search/${searchText}`;
+    try{
+        const response = await fetch(url);
+        allData = await response.json();
+        if(allData.response === 'success'){
+            // console.log(allData);
+            showSearchList(allData.results);
         }
-      });
-      renderResults(results);
-      console.log(results);
-    });
+    } catch(error){
+        console.log(error);
+    }
 }
 
-function renderResults(results) {
-  if (results.length === 0) { // check if results is empty
+const showSearchList = (data) => {
     searchList.innerHTML = "";
-    homeText.style.display = "block"; // show the home text element
-    return;
-  }
-  homeText.style.display = "none"; // hide the home text element
-  searchList.innerHTML = ""; // clear previous results
-  results.slice(0, 3).forEach(result => {
-    const searchElement = document.createElement("span");
-    searchElement.innerText = result.name;
-
-    const imageElement = document.createElement("img");
-    imageElement.src = result.image;
-    imageElement.style.width = "80px"; // set image width
-
-    const buttonElement = document.createElement("button");
-    buttonElement.appendChild(imageElement);
-    buttonElement.appendChild(searchElement);
-
-    buttonElement.addEventListener("click", () => {
-      homeText.style.display = "none";
+    data.forEach(dataItem => {
+        const divElem = document.createElement('div');
+        divElem.classList.add('search-list-item');
+        divElem.innerHTML = `
+            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
+            <p data-id = "${dataItem.id}">${dataItem.name}</p>
+        `;
+        searchList.appendChild(divElem);
     });
-
-    searchList.appendChild(buttonElement);
-  });
 }
-searchInput.addEventListener("keyup", () => {
-  searchCharacter(searchInput.value);
+
+searchForm.search.addEventListener('keyup', () => {
+    if(searchForm.search.value.length > 1){
+        fetchAllSuperHero(searchForm.search.value);
+    } else {
+        searchList.innerHTML = "";
+    }
 });
 
-window.onload = () => {
-  renderResults([]);
-}; 
+searchList.addEventListener('click', (event) => {
+    let searchId = event.target.dataset.id;
+    let singleData = allData.results.filter(singleData => {
+        return searchId === singleData.id;
+    })
+    showSuperheroDetails(singleData);
+    searchList.innerHTML = "";
+});
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ29kZGFyZGNvbGUiLCJhIjoiY2xmNXB3MWg4MDVlZjNwcW1ncXF5b3dncCJ9.0Ft4_LAHrAwshgLHvm4r0Q';
+const showSuperheroDetails = (data) => {
+    console.log(data);
+    document.querySelector('.app-body-content-thumbnail').innerHTML = `
+        <img src = "${data[0].image.url}">
+    `;
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-    enableHighAccuracy: true
-})
+    document.querySelector('.name').textContent = data[0].name;
+    document.querySelector('.powerstats').innerHTML = `
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>intelligence</span>
+        </div>
+        <span>${data[0].powerstats.intelligence}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>strength</span>
+        </div>
+        <span>${data[0].powerstats.strength}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>speed</span>
+        </div>
+        <span>${data[0].powerstats.speed}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>durability</span>
+        </div>
+        <span>${data[0].powerstats.durability}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>power</span>
+        </div>
+        <span>${data[0].powerstats.power}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>combat</span>
+        </div>
+        <span>${data[0].powerstats.combat}</span>
+    </li>
+    `;
 
-function successLocation(position) {
-    console.log(position)
-    setupMap([position.coords.longitude, position.coords.latitude])
+    document.querySelector('.biography').innerHTML = `
+    <li>
+        <span>full name</span>
+        <span>${data[0].biography['full-name']}</span>
+    </li>
+    <li>
+        <span>alert-egos</span>
+        <span>${data[0].biography['alter-egos']}</span>
+    </li>
+    <li>
+        <span>aliases</span>
+        <span>${data[0].biography['aliases']}</span>
+    </li>
+    <li>
+        <span>place-of-birth</span>
+        <span>${data[0].biography['place-of-birth']}</span>
+    </li>
+    <li>
+        <span>first-apperance</span>
+        <span>${data[0].biography['first-appearance']}</span>
+    </li>
+    <li>
+        <span>publisher</span>
+        <span>${data[0].biography['publisher']}</span>
+    </li>
+    `;
+
+    document.querySelector('.appearance').innerHTML = `
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> gender
+        </span>
+        <span>${data[0].appearance['gender']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> race
+        </span>
+        <span>${data[0].appearance['race']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> height
+        </span>
+        <span>${data[0].appearance['height'][0]}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> weight
+        </span>
+        <span>${data[0].appearance['weight'][0]}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> eye-color
+        </span>
+        <span>${data[0].appearance['eye-color']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> hair-color
+        </span>
+        <span>${data[0].appearance['hair-color']}</span>
+    </li>
+    `;
+
+    document.querySelector('.connections').innerHTML = `
+    <li>
+        <span>group--affiliation</span>
+        <span>${data[0].connections['group-affiliation']}</span>
+    </li>
+    <li>
+        <span>relatives</span>
+        <span>${data[0].connections['relatives']}</span>
+    </li>
+    `;
 }
-
-function errorLocation() {
-    setupMap([-2.24, 53.48])
-}
-
-function setupMap(center) {
-const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11'
-        })
-
-        const nav = new mapboxgl.NavigationControl()
-        map.addControl(nav)
-
-        var directions = new MapboxDirections({
-            accessToken: mapboxgl.accessToken
-          });
-          
-          map.addControl(directions, 'top-left');
-    }
