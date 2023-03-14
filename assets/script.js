3052b3bc6c00d7e39acbd6cc41cb2b1be1c0562d
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-03-13 20:10:19 -0400

    working with Jack

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,16 @@
+var homeLink = document.querySelector(".home-header");
+var charactersLink = document.querySelector(".characters-header");
+var compareLink = document.querySelector(".compare-header");
+var searchForm = document.querySelector(".container-header-search");
+var searchInput = document.querySelector(".container-header-search input");
+var searchList = document.getElementById("search-list");
+
 let activeTab = 1, allData;
 
 const init = () => {
     showActiveTabBody();
     showActiveTabHead();
 }
-
 const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
 
 const showActiveTabBody = () => {
@@ -15,19 +21,40 @@ const showActiveTabBody = () => {
 const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
 const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
 
-// even listeners
-window.addEventListener('DOMContentLoaded', () => init());
-// button event listeners
-allTabsHead.forEach(singleTabHead => {
-    singleTabHead.addEventListener('click', () => {
-        hideAllTabHead();
-        activeTab = singleTabHead.dataset.id;
-        showActiveTabHead();
-        showActiveTabBody();
-    });
+
+// Gets the home text element
+var homeText = document.querySelector(".home-text");
+
+// Hides certain elements by default
+homeLink.style.display = "none";
+searchForm.style.display = "none";
+
+// Adds event listener to the characters link
+charactersLink.addEventListener("click", () => {
+  homeLink.style.display = "inline";
+  charactersLink.style.display = "none";
+  searchForm.style.display = "block";
 });
 
-const getInputValue = (event) => {
+// Adds event listener to the compare link
+compareLink.addEventListener("click", () => {
+  alert("Sorry, comparison feature is not available yet. Stay tuned!");
+});
+
+// Adds event listener to the home link
+homeLink.addEventListener("click", () => {
+  location.replace(location.href);
+  homeLink.style.display = "none";
+  charactersLink.style.display = "inline";
+});
+
+function searchCharacter(searchText) {
+  if (!searchText.trim()) { // check if searchText is empty or only whitespace
+    searchList.innerHTML = "";
+    homeText.style.display = "block"; // show the home text element
+    return;
+  }
+  const getInputValue = (event) => {
     event.preventDefault();
     let searchText = searchForm.search.value;
     fetchAllSuperHero(searchText);
@@ -36,36 +63,61 @@ const getInputValue = (event) => {
 // search form submission
 searchForm.addEventListener('submit', getInputValue);
 
-const showSearchList = (data) => {
-    searchList.innerHTML = "";
-    data.forEach(dataItem => {
-        const divElem = document.createElement('div');
-        divElem.classList.add('search-list-item');
-        divElem.innerHTML = `
-            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
-            <p data-id = "${dataItem.id}">${dataItem.name}</p>
-        `;
-        searchList.appendChild(divElem);
+//Api Key
+  const url = `https://www.superheroapi.com/api.php/2333600500149305/search/${searchText}`
+  fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+      const results = jsonData.results.map(element => {
+        return {
+          name: element.name,
+          image: element.image.url
+        }
+      });
+      renderResults(results);
+      console.log(results);
     });
 }
 
-searchForm.search.addEventListener('keyup', () => {
-    if(searchForm.search.value.length > 1){
-        fetchAllSuperHero(searchForm.search.value);
-    } else {
-        searchList.innerHTML = "";
-    }
-});
-
-searchList.addEventListener('click', (event) => {
-    let searchId = event.target.dataset.id;
-    let singleData = allData.results.filter(singleData => {
-        return searchId === singleData.id;
-    })
-    showSuperheroDetails(singleData);
+function renderResults(results) {
+  if (results.length === 0) { // check if results is empty
     searchList.innerHTML = "";
+    homeText.style.display = "block"; // show the home text element
+    return;
+  }
+  homeText.style.display = "none"; // hide the home text element
+  searchList.innerHTML = ""; // clear previous results
+  results.slice(0, 3).forEach(result => {
+    const searchElement = document.createElement("span");
+    searchElement.innerText = result.name;
+
+    const imageElement = document.createElement("img");
+    imageElement.src = result.image;
+    imageElement.style.width = "80px"; // set image width
+
+    const buttonElement = document.createElement("button");
+    buttonElement.appendChild(imageElement);
+    buttonElement.appendChild(searchElement);
+
+    buttonElement.addEventListener("click", () => {
+      homeText.style.display = "none";
+    });
+
+    searchList.appendChild(buttonElement);
+  });
+}
+searchInput.addEventListener("keyup", () => {
+  searchCharacter(searchInput.value);
 });
 
+window.onload = () => {
+  renderResults([]);
+}; 
+
+//END West
+
+
+//Start Cam
 const showSuperheroDetails = (data) => {
     console.log(data);
     document.querySelector('.app-body-content-thumbnail').innerHTML = `
@@ -195,3 +247,35 @@ const showSuperheroDetails = (data) => {
     </li>
     `;
 }
+
+//Start Cole
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ29kZGFyZGNvbGUiLCJhIjoiY2xmNXB3MWg4MDVlZjNwcW1ncXF5b3dncCJ9.0Ft4_LAHrAwshgLHvm4r0Q';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successLocation(position) {
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        })
+
+        const nav = new mapboxgl.NavigationControl()
+        map.addControl(nav)
+
+        var directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+          });
+          
+          map.addControl(directions, 'top-left');
+    }
