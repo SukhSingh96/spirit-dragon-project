360ece4f924bac6144ba85f292f8868f4e7dfd38
Sukhdip Singh <121911679+SukhSingh96@users.noreply.github.com>
2023-03-14 17:02:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Details from "./pages/Details";
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/login" component={Login} />
+				<Route exact path="/" component={Home} />
+				<Route path="/search" component={Home} />
+				<Route path="/hero" component={Details} />
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
